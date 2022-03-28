697755fcc85595972024f13ecb54917b44fbb36f
Fabio Veroli <fabio.veroli@studio.unibo.it>
2022-03-28 18:20:58 +0200

    Created classthat implements EntityGenerator

---
+++ src/main/model/generator/EntityGeneratorImpl.java
@@ -0,0 +1,41 @@
+package main.model.generator;
+
+import java.util.List;
+
+import main.model.entity.DynamicEntity;
+
+public class EntityGeneratorImpl implements EntityGenerator {
+
+    private List<DynamicEntity> entityList;
+
+    @Override
+    public List<DynamicEntity> getEntityList() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void updateList() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void addEntity() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void removeElement() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public boolean checkPosition() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+}
