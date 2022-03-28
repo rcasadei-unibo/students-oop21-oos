7650f246c50e7170527c37f1baa816e8252f6523
Fabio Veroli <fabio.veroli@studio.unibo.it>
2022-03-28 15:26:26 +0200

    Fix package structure

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,11 +1,13 @@
 package main;
 
 import javafx.application.Application;
+import view.ViewImpl;
 
 public class Main {
 
     public static void main(final String[] args) {
-        
-    }
 
+        Application.launch(ViewImpl.class, args);
+
+    }
 }
