9e1dae3ee18d4a5ea9b8943c33d193850c162758
Fabio Veroli <fabio.veroli@studio.unibo.it>
2022-03-28 23:38:24 +0200

    Implemented methods and add constructor

--- src/main/model/generator/EntityGeneratorImpl.java
+++ src/main/model/generator/EntityGeneratorImpl.java
@@ -1,34 +1,51 @@
 package main.model.generator;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
+import javafx.geometry.Dimension2D;
 import main.model.entity.DynamicEntity;
 
 public class EntityGeneratorImpl implements EntityGenerator {
 
-    private List<DynamicEntity> entityList;
+    private final List<DynamicEntity> entityList;
+    private int entityCount;
+    private final Dimension2D worldDimension;
+
+    public EntityGeneratorImpl(final Dimension2D worldDimension) {
+        this.entityList = new ArrayList<>();
+        //this.entityList.add(new Obstacle(null, worldDimension));
+        this.entityCount = 0;
+        this.worldDimension = worldDimension;
+    }
 
     @Override
     public List<DynamicEntity> getEntityList() {
-        // TODO Auto-generated method stub
-        return null;
+        return Collections.unmodifiableList(entityList);
     }
 
     @Override
     public void updateList() {
-        // TODO Auto-generated method stub
+        this.removeEntity();
+        if (this.entityList.isEmpty() || this.checkPosition()) {
+            this.addEntity();
+        }
+        this.entityList.forEach(e -> e.updatePosition());
 
     }
 
     @Override
     public void addEntity() {
         // TODO Auto-generated method stub
-
+        this.entityCount++;
     }
 
     @Override
-    public void removeElement() {
-        // TODO Auto-generated method stub
+    public void removeEntity() {
+        if (!this.entityList.isEmpty() && this.entityList.get(0).isOutofScreen()) {
+            this.entityList.remove(0);
+        }
 
     }
 
