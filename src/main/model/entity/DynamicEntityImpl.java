808d0989c19f29173ca2541a4e610c2383e71df1
Fabio Veroli <fabio.veroli@studio.unibo.it>
2022-03-30 17:12:01 +0200

    Implemented the new method added in the interface

--- src/main/model/entity/DynamicEntityImpl.java
+++ src/main/model/entity/DynamicEntityImpl.java
@@ -5,38 +5,56 @@ import java.awt.geom.Point2D.Double;
 
 import javafx.geometry.Dimension2D;
 import javafx.geometry.Rectangle2D;
+import javafx.scene.image.Image;
 
-public class DynamicEntityImpl implements DynamicEntity {
+public abstract class DynamicEntityImpl implements DynamicEntity {
 
     private static final  double INITIAL_SPEEDX = 2.5;
     private final Point2D.Double coordinates;
     private final Dimension2D dimensions;
     private double speedX;
+    private Image image;
+    private final EntityLevelType levelType;
 
-    public DynamicEntityImpl(final Double coordinates, final Dimension2D dimensions) {
+    public DynamicEntityImpl(final Double coordinates, final Dimension2D dimensions, final EntityLevelType levelType) {
         this.coordinates = coordinates;
         this.dimensions = dimensions;
+        this.levelType = levelType;
         this.speedX = INITIAL_SPEEDX;
     }
 
     @Override
-    public void updatePosition() {
+    public final void updatePosition() {
         this.coordinates.setLocation(coordinates.getX() - speedX, coordinates.getY());
     }
 
     @Override
-    public Rectangle2D getBounds() {
+    public final Rectangle2D getBounds() {
         return new Rectangle2D(coordinates.getX(), coordinates.getY(), dimensions.getWidth(), dimensions.getHeight());
     }
 
     @Override
-    public void setSpeedX(final double speedX) {
+    public final void setSpeedX(final double speedX) {
         this.speedX = speedX;
     }
 
     @Override
-    public boolean isOutofScreen() {
+    public final boolean isOutofScreen() {
         return this.coordinates.getX() < -this.dimensions.getWidth(); 
     }
 
+    @Override
+    public final void setImage(final Image image) {
+        this.image = image;
+    }
+
+    @Override
+    public final Image getImage() {
+        return this.image;
+    }
+
+    @Override
+    public final EntityLevelType getLevelType() {
+        return this.levelType;
+    }
 }
