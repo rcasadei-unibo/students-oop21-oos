4f688938fe22ea179dac87e84041ed44a5669216
Fabio Veroli <fabio.veroli@studio.unibo.it>
2022-03-30 18:46:24 +0200

    Remove methods from interface

--- src/main/model/generator/EntityGenerator.java
+++ src/main/model/generator/EntityGenerator.java
@@ -17,19 +17,4 @@ public interface EntityGenerator {
      */
     void updateList();
 
-    /**
-     * Add a DynamicEntity to the list.
-     */
-    void addEntity();
-
-    /**
-     * Remove entity from the list, if it isn't empty and the entity is out of the screen.
-     */
-    void removeEntity();
-
-    /**
-     * 
-     * @return true if the distance to spawn a new entity is , false otherwise. 
-     */
-    boolean checkPosition();
 }
