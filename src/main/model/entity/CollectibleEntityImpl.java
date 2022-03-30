fa193c268b710fc13b613f2637c2b43b33d9c04f
Fabio Veroli <fabio.veroli@studio.unibo.it>
2022-03-30 17:25:59 +0200

    Create abstarct class that implement CollectibleEntity

---
+++ src/main/model/entity/CollectibleEntityImpl.java
@@ -0,0 +1,26 @@
+package main.model.entity;
+
+import java.awt.geom.Point2D.Double;
+
+import javafx.geometry.Dimension2D;
+
+public abstract class CollectibleEntityImpl extends DynamicEntityImpl implements CollectibleEntity {
+
+    private boolean collected;
+
+    public CollectibleEntityImpl(final Double coordinates, final Dimension2D dimensions, final EntityLevelType level) {
+        super(coordinates, dimensions, level);
+        this.collected = false;
+    }
+
+    @Override
+    public final void setCollected(final boolean collectedState) {
+        this.collected = collectedState;
+    }
+
+    @Override
+    public final boolean wasCollected() {
+        return this.collected;
+    }
+
+}
