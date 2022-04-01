8deec4548bdd47d369c9ca52ce6e8053a6d421fc
Davide Tonelli <davide.tonelli8@studio.unibo.it>
2022-04-01 13:41:11 +0200

    Add getEntities method

--- src/main/model/GameState.java
+++ src/main/model/GameState.java
@@ -1,8 +1,14 @@
 package main.model;
 
+import java.util.List;
+
+import main.model.entity.DynamicEntity;
+
 public interface GameState {
 
     Player getPlayer();
+
+    List<DynamicEntity> getEntities();
 
     boolean isGameOver();
 
