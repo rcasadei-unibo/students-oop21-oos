02dcb992994754bb61cafe46c02b1af0bdfa0116
Fabio Veroli <fabio.veroli@studio.unibo.it>
2022-04-05 10:21:09 +0200

    Create enum for describe an entity's type

---
+++ src/main/java/model/entity/EntityType.java
@@ -0,0 +1,30 @@
+package model.entity;
+
+public enum EntityType {
+
+    /**
+     * Obstacle's type.
+     */
+    OBSATCLE(5.0), 
+
+    /**
+     * Platform's type.
+     */
+    PLATFORM(0.0), 
+
+    /**
+     * Coin's type.
+     */
+    COIN(5.0);
+
+    private final double distanceFactor;
+
+    EntityType(final double distanceFactor) {
+        this.distanceFactor = distanceFactor;
+    }
+
+    public double getDistanceFactor() {
+        return this.distanceFactor;
+    }
+
+}
