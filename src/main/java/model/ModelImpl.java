bbccd37af4ee3ecc00cb671f0028c47efe6761d0
Davide Tonelli <davide.tonelli8@studio.unibo.it>
2022-04-06 16:39:12 +0200

    Fix ModelImpl merge problem

---
+++ src/main/java/model/ModelImpl.java
@@ -0,0 +1,29 @@
+package model;
+
+public class ModelImpl implements Model {
+
+    private GameState gameState;
+    private Statistics statistics;
+
+    public ModelImpl() {
+        this.gameState = new GameStateImpl();
+        this.statistics = new StatisticsImpl();
+    }
+
+    @Override
+    public GameState getGameState() {
+        return this.gameState;
+    }
+
+    @Override
+    public Statistics getStatistics() {
+        return this.statistics;
+    }
+
+    @Override
+    public void update() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
