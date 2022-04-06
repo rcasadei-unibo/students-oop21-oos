a7621a4189e48e6274c2fb459766e903b5227398
Rachele Margutti <rachele.margutti@studio.unibo.it>
2022-04-06 17:08:44 +0200

    Create a timer used by some powerups

---
+++ src/main/java/model/entity/powerup/EffectTimer.java
@@ -0,0 +1,22 @@
+package model.entity.powerup;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class EffectTimer {
+
+    private final Timer time; 
+    private static final long DELAY = 10000L; 
+
+    public EffectTimer() {
+        time = new Timer(); 
+    }
+
+    public final void scheduleTask(final TimerTask powerupTask) {
+
+        time.schedule(powerupTask, DELAY);
+        time.cancel();
+
+    }
+
+}
