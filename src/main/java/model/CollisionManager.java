e0587ac6455121b5f53954d96e3b81436d9944c4
Sara Cappelletti <sara.cappelletti4@studio.unibo.it>
2022-04-07 19:15:48 +0200

    Changed parametherd in playersCollidesWidth and renamed it

--- src/main/java/model/CollisionManager.java
+++ src/main/java/model/CollisionManager.java
@@ -1,11 +1,16 @@
 package model;
 
+import java.util.List;
+
+import model.entity.DynamicEntityImpl;
+
 public interface CollisionManager {
 
     /**
-     * 
+     * @param pl the Player
+     * @param objects the list of Objects
      * @param model the Model
      */
-    void playerCollidesWidth(Model model);
+    void playerCollidesWidth(Player pl, List<DynamicEntityImpl> objects, Model model);
 
 }
--- src/main/java/model/CollisionManagerImpl.java
+++ src/main/java/model/CollisionManagerImpl.java
@@ -1,6 +1,5 @@
 package model;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import model.entity.DynamicEntityImpl;
@@ -8,16 +7,8 @@ import model.entity.EntityType;
 
 public final class CollisionManagerImpl implements CollisionManager {
 
-    private Player pl;
-    private List<DynamicEntityImpl> objects = new ArrayList();
-
-    public CollisionManagerImpl(final Player pl, final List<DynamicEntityImpl> objects) {
-        this.pl = pl;
-        this.objects = objects;
-    }
-
     @Override
-    public void playerCollidesWidth(final Model model) {
+    public void playerCollidesWidth(final Player pl, final List<DynamicEntityImpl> objects, final Model model) {
         for (final DynamicEntityImpl obj : objects) {
             if (obj.getBounds().intersects(pl.getBounds())) {
                 if (obj.getType() == EntityType.PLATFORM)  {
