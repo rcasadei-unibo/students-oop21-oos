b2e70e8a161e7bc120d5678ee4f8d69463a34d06
Rachele Margutti <rachele.margutti@studio.unibo.it>
2022-04-08 15:54:17 +0200

    Create Shop view

--- src/main/java/view/ShopViewImpl.java
+++ src/main/java/view/ShopViewImpl.java
@@ -1,9 +1,18 @@
 package view;
 
+import javafx.geometry.Rectangle2D;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Shape;
 import javafx.scene.text.Font;
 import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
@@ -13,6 +22,7 @@ public class ShopViewImpl implements ShopView {
     private final View view; 
     private final Stage stage; 
     private final Pane pane; 
+    private Image shopWallpaper; 
 
     //Check all of the sizes!!!
     private static final int FONT_SIZE = 18; 
@@ -24,10 +34,8 @@ public class ShopViewImpl implements ShopView {
     private static final int ARROW_BUTTON_X = 240; 
     private static final int ARROW_DXBUTTON_X = 510; 
     private static final int ARROW_BUTTON_Y = 200; 
-    private static final int SKIN_BUTTON_WIDTH = 250; 
-    private static final int SKIN_BUTTON_X = 180; 
-    private static final int SKIN_BUTTON_Y = 50; 
     private static final int ARROW_HEIGTH = 40; 
+    private static final int SHOPTITLE_X = 330; 
 
     public ShopViewImpl(final View view, final Stage stage, final Pane pane) {
         super(); 
@@ -83,10 +91,36 @@ public class ShopViewImpl implements ShopView {
         sxArrow.setPrefWidth(ARROW_WIDTH);
         sxArrow.setGraphic(sxArr);
 
+        final ImageView title = new ImageView(new Image("Shop.png"));
+        title.setLayoutX(SHOPTITLE_X);
+        title.setLayoutY(0); 
+
+        final ImageView programmerSkin = new ImageView(new Image("ProgrammerSkin.png")); 
+        programmerSkin.setLayoutX(357);
+        programmerSkin.setLayoutY(130);
+
+        final Rectangle rectangle = new Rectangle(345, 120, 120, 170); 
+        rectangle.setFill(Color.LIGHTPINK);
+        rectangle.setArcHeight(20);
+        rectangle.setArcWidth(20);
+        final Rectangle outerRec = new Rectangle(340, 115, 130, 180); 
+        outerRec.setFill(Color.BLACK);
+        outerRec.setArcHeight(20);
+        outerRec.setArcWidth(20);
+
+        this.shopWallpaper = new Image("ShopBackground.jpg", 854, 480, false, true); 
+        final BackgroundImage shopBackground = new BackgroundImage(shopWallpaper, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, BackgroundSize.DEFAULT); 
+
         this.pane.getChildren().add(buy); 
         this.pane.getChildren().add(select); 
         this.pane.getChildren().add(dxArrow); 
         this.pane.getChildren().add(sxArrow); 
+        this.pane.getChildren().add(title); 
+        this.pane.getChildren().add(outerRec); 
+        this.pane.getChildren().add(rectangle);
+        this.pane.getChildren().add(programmerSkin); 
+
+        this.pane.setBackground(new Background(shopBackground));
 
     }
 
