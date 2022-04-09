0d9fef65306159f57a3de03c238afd6874ae1172
Davide Tonelli <davide.tonelli8@studio.unibo.it>
2022-04-09 15:53:14 +0200

    Add field CollisionManager

--- src/main/java/model/ModelImpl.java
+++ src/main/java/model/ModelImpl.java
@@ -1,13 +1,21 @@
 package model;
 
+import java.util.List;
+
+import model.entity.DynamicEntity;
+
 public class ModelImpl implements Model {
 
-    private GameState gameState;
-    private Statistics statistics;
+    private final GameState gameState;
+    private final Statistics statistics;
+    private final CollisionManager collisionManager;
+    //private boolean isGameOVer;
 
     public ModelImpl() {
         this.gameState = new GameStateImpl();
         this.statistics = new StatisticsImpl();
+        this.collisionManager = new CollisionManagerImpl();
+        //this.isGameOVer = false;
     }
 
     @Override
@@ -23,6 +31,11 @@ public class ModelImpl implements Model {
     @Override
     public void update() {
         this.gameState.update();
+
+        final Player player = this.gameState.getPlayer();
+        final List<DynamicEntity> entities = this.gameState.getEntities();
+        this.collisionManager.playerCollidesWidth(player, entities, this);
+
         this.statistics.update();
     }
 
