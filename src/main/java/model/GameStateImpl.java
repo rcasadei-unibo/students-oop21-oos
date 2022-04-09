d36155f77870cc1903d857fdf6bcec579afc8f5e
Davide Tonelli <davide.tonelli8@studio.unibo.it>
2022-04-09 15:07:27 +0200

    Add implementation update method

--- src/main/java/model/GameStateImpl.java
+++ src/main/java/model/GameStateImpl.java
@@ -1,18 +1,21 @@
 package model;
 
-import java.util.LinkedList;
 import java.util.List;
 
+import javafx.geometry.Dimension2D;
 import model.entity.DynamicEntity;
+import model.generator.EntityGenerator;
+import model.generator.EntityGeneratorImpl;
 
 public class GameStateImpl implements GameState {
 
     private Player player;
+    private EntityGenerator entityGenerator;
     private List<DynamicEntity> entities;
 
     public GameStateImpl() {
         this.player = new PlayerImpl();
-        this.entities = new LinkedList<>();
+        this.entityGenerator = new EntityGeneratorImpl(new Dimension2D(854, 480));
     }
 
     @Override
@@ -22,13 +25,19 @@ public class GameStateImpl implements GameState {
 
     @Override
     public List<DynamicEntity> getEntities() {
-        return this.entities;
+        return this.entityGenerator.getEntities();
     }
 
     @Override
     public boolean isGameOver() {
         // TODO Auto-generated method stub
         return false;
+    }
+
+    @Override
+    public void update() {
+        this.player.updateJump();
+        this.entityGenerator.updateList();
     }
 
 }
