8d676fb8ef1a33626df575d7eaeec0ea4bb7f42a
Davide Tonelli <davide.tonelli8@studio.unibo.it>
2022-04-12 11:18:22 +0200

    Add StatisticsUpdater class

---
+++ src/main/java/model/StatisticsUpdater.java
@@ -0,0 +1,35 @@
+package model;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class StatisticsUpdater implements Runnable {
+
+    private static final int TIME_RATE_IN_SECONDS = 1;
+    private final ScheduledExecutorService executor;
+    private final Statistics statistics;
+
+    public StatisticsUpdater(final Statistics statistics) {
+        super();
+        this.executor = Executors.newScheduledThreadPool(1);
+        this.executor.scheduleAtFixedRate(this, 0, TIME_RATE_IN_SECONDS, TimeUnit.SECONDS);
+        this.statistics = statistics;
+    }
+
+    @Override
+    public void run() {
+        this.statistics.update();
+        //System.out.println("Distance: " + this.statistics.getDistance());
+    }
+
+    public void start() {
+        this.run();
+    }
+
+    public void stop() {
+        this.executor.shutdown();
+        //this.statistics.saveOnFile();
+    }
+
+}
