4aff56d8523d981377a178a416f29314710adc35
Rachele Margutti <rachele.margutti@studio.unibo.it>
2022-04-13 16:13:33 +0200

    Implement the Mushroom powerup effect

--- src/main/java/model/entity/powerup/Mushroom.java
+++ src/main/java/model/entity/powerup/Mushroom.java
@@ -5,6 +5,7 @@ import java.util.TimerTask;
 
 import javafx.scene.image.Image;
 import model.Model;
+import model.entity.Coin;
 import model.entity.DynamicEntityImpl;
 import model.entity.EntityLevel;
 import model.entity.EntityType;
@@ -12,6 +13,8 @@ import model.entity.EntityType;
 public class Mushroom extends DynamicEntityImpl {
 
     private final EffectTimer mushroomTimer; 
+    private static final int DOUBLECOIN = 2; 
+    private static final int STDCOIN = 1; 
 
     public Mushroom(final Double coordinates, final Image image, final EntityLevel level, final EntityType type) {
         super(coordinates, image, level, type);
@@ -20,8 +23,7 @@ public class Mushroom extends DynamicEntityImpl {
 
     @Override
     public void activateEffect(final Model model) {
-        // TODO Auto-generated method stub
-        //EFFETTO
+        Coin.setCoinValue(DOUBLECOIN);
         mushroomTask(model); 
     }
 
@@ -29,7 +31,7 @@ public class Mushroom extends DynamicEntityImpl {
         final TimerTask task = new TimerTask() {
             @Override
             public void run() {
-                //AZIONE PER QUANDO TORNA NORMALE 
+                Coin.setCoinValue(STDCOIN);
             }
         };
         mushroomTimer.scheduleTask(task);
