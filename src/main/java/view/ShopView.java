87deb42ec3263681808a6a8adde46b09891d08f3
Rachele Margutti <rachele.margutti@studio.unibo.it>
2022-04-14 15:00:22 +0200

    Modify render method parameters

--- src/main/java/view/ShopView.java
+++ src/main/java/view/ShopView.java
@@ -1,7 +1,13 @@
 package view;
 
+import model.shop.Shop;
+
 public interface ShopView {
 
+    /**
+     * Create the view for the shop menu.
+     * @param shop the shop class with its methods. 
+     */
     void render(); 
 
 }
