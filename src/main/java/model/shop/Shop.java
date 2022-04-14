1747914c598182e8c5bc042ad11529061f9b083a
Rachele Margutti <rachele.margutti@studio.unibo.it>
2022-04-14 10:26:52 +0200

    Add Statistics field

--- src/main/java/model/shop/Shop.java
+++ src/main/java/model/shop/Shop.java
@@ -2,15 +2,17 @@ package model.shop;
 
 import java.util.List;
 
+import model.Statistics;
+
 public interface Shop {
 
     /**
      * If the player has enough money, update the player properties.
      * @param selectedItem the item chosen by the player.
      */
-    void shopItemPayment(ShopItem selectedItem);
+    void shopItemPayment(ShopItem selectedItem, Statistics stats);
 
-    void misteryBoxPayment(MysteryBox box); 
+    void misteryBoxPayment(MysteryBox box, Statistics stats); 
 
     void printItems(); 
 
