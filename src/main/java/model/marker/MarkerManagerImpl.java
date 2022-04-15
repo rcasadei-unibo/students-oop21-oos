f499c2881f88cc65e17570c44fa03bbe00d95375
Davide Tonelli <davide.tonelli8@studio.unibo.it>
2022-04-15 11:59:59 +0200

    MarkrManagerImpl implemented

--- src/main/java/model/marker/MarkerManagerImpl.java
+++ src/main/java/model/marker/MarkerManagerImpl.java
@@ -3,50 +3,71 @@ package model.marker;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class MarkerManagerImpl implements MarkerManager {
 
     private static final int DISTANCE_BETWEEN_MARKERS = 100;
-    private final double lastDeathDistance;
-    private final double recordDistance;
+    private final int lastDeathDistance;
+    private final int recordDistance;
     private int numberNextMarker;
     private final MarkerFactory markerFactory;
+    private Optional<Marker> lastDeathMarker;
+    private Optional<Marker> recordMarker;
     private List<Optional<Marker>> markers;
 
-
-
-    public MarkerManagerImpl(final double lastDeathDistance, final double recordDistance) {
+    public MarkerManagerImpl(final int lastDeathDistance, final int recordDistance) {
         super();
         this.lastDeathDistance = lastDeathDistance;
         this.recordDistance = recordDistance;
         this.numberNextMarker = 1;
-        //this.markerFactory = new MarkerFactoryImpl();
+        this.markerFactory = new MarkerFactoryImpl();
+        this.lastDeathMarker = Optional.empty();
+        this.recordMarker = Optional.empty();
         this.markers = new ArrayList<>();
     }
 
     @Override
-    public boolean isCommonMarkerToBeCreated(int distance) {
-        // TODO Auto-generated method stub
+    public boolean isCommonMarkerToBeCreated(final int distance) {
+        final int nextMarkerDist = this.numberNextMarker * DISTANCE_BETWEEN_MARKERS;
+
+        if (distance >= nextMarkerDist && distance % nextMarkerDist >= 0) {
+            this.numberNextMarker++;
+            return true;
+        }
         return false;
     }
 
     @Override
-    public void update() {
-        // TODO Auto-generated method stub
-        
+    public void check(final int distance) {
+        if (this.isCommonMarkerToBeCreated(distance)) {
+            this.markers.add(Optional.of(this.markerFactory.createCommonMarker()));
+        }
+
+        if (this.lastDeathMarker.isEmpty() && distance > 0 && distance >= this.lastDeathDistance) {
+            this.lastDeathMarker = Optional.of(this.markerFactory.createLastDeathMarker());
+            this.markers.add(lastDeathMarker);
+        }
+
+        if (this.recordMarker.isEmpty() && distance > 0 && distance >= this.recordDistance) {
+            this.recordMarker = Optional.of(this.markerFactory.createRecordMarker());
+            this.markers.add(recordMarker);
+        }
     }
 
     @Override
-    public void check() {
-        // TODO Auto-generated method stub
-        
-    }
+    public void update(final double difficulty) {
+        this.markers = this.markers.stream()
+                .filter(op -> !op.get().isOutOfScreen())
+                .collect(Collectors.toList());
 
+        this.markers.stream()
+                .forEach(op -> op.get().update(difficulty));
+    }
 
     @Override
     public List<Optional<Marker>> getMarkers() {
-        // TODO Auto-generated method stub
-        return null;
+        return this.markers;
     }
 
 }
