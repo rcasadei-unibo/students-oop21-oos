c012a10f2e06ff5c7e91410822ccef4111f943af
Rachele Margutti <rachele.margutti@studio.unibo.it>
2022-04-19 16:37:44 +0200

    Implement read and write file methods

--- src/main/java/model/shop/ShopImpl.java
+++ src/main/java/model/shop/ShopImpl.java
@@ -1,7 +1,14 @@
 package model.shop;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import model.Statistics;
@@ -13,11 +20,14 @@ import model.StatisticsImpl;
  */
 public class ShopImpl implements Shop {
 
+    private static final String SEP = File.separator;
+    private static final String FILE_NAME = System.getProperty("user.home") + SEP + "OOS_shopItems.txt";
     private final List<ShopItem> items = new ArrayList<>();
-    private final List<ShopItem> purchasedItems = new ArrayList<>();
+    private List<ShopItem> purchasedItems = new ArrayList<>();
 
     public ShopImpl() {
         Stream.of(Skins.values()).forEach(i -> items.add(new ShopItemImpl(i.name(), i.getPrice())));
+        readPurchasedItems();
     }
 
     /**
@@ -66,18 +76,55 @@ public class ShopImpl implements Shop {
     }
 
     /**
-     * @return the items
+     * {@inheritDoc}
      */
     public List<ShopItem> getItems() {
         return items;
     }
 
     /**
-     *
-     * @return the list of the purchased items
+     *{@inheritDoc}
      */
     public List<ShopItem> getPurchasedItems() {
         return purchasedItems;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void saveShopItem() throws IOException {
+        try (BufferedWriter bw = Files.newBufferedWriter(Paths.get(FILE_NAME))) {
+            items.stream()
+            .forEach(elem -> {
+                try {
+                    if (purchasedItems.contains(elem)) {
+                        bw.write("1"); 
+                        bw.newLine(); 
+                    } else {
+                        bw.write("0"); 
+                        bw.newLine(); 
+                    }
+                } catch (IOException e) {
+                    System.out.println("Error in saveShopItem");
+                }
+            });
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void readPurchasedItems() {
+        try (BufferedReader br = Files.newBufferedReader(Paths.get(FILE_NAME))) {
+            List<String> filesItems = br.lines().collect(Collectors.toList()); 
+            Stream.iterate(0, i -> i + 1).limit(items.size()).forEach(i -> {
+                if (filesItems.get(i).equals("1")) {
+                    purchasedItems.add(items.get(i));
+                }
+            });
+        } catch (IOException e) {
+            purchasedItems = new ArrayList<>(); 
+        }
     }
 
 }
