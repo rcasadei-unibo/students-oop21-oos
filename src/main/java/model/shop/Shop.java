204762ecf3700ca40c7f4a167412b26cce83d8e0
Rachele Margutti <rachele.margutti@studio.unibo.it>
2022-04-19 16:37:12 +0200

    Add new methods to the Shop interface

--- src/main/java/model/shop/Shop.java
+++ src/main/java/model/shop/Shop.java
@@ -1,5 +1,6 @@
 package model.shop;
 
+import java.io.IOException;
 import java.util.List;
 
 import model.Statistics;
@@ -9,13 +10,40 @@ public interface Shop {
     /**
      * If the player has enough money, update the player properties.
      * @param selectedItem the item chosen by the player.
+     * @param stats the game statistics. 
      */
     void shopItemPayment(ShopItem selectedItem, Statistics stats);
 
+    /**
+     * If the player has enough money, 
+     * @param box
+     * @param stats
+     */
     void misteryBoxPayment(MysteryBox box, Statistics stats); 
 
     void printItems(); 
 
+    /**
+     * 
+     * @return the items of the shop.
+     */
     List<ShopItem> getItems();
+
+    /**
+     * 
+     * @return the purchased items. 
+     */
+    List<ShopItem> getPurchasedItems(); 
+
+    /**
+     * 
+     */
+    void saveShopItem() throws IOException ; 
+
+    /**
+     * 
+     * @return the saved purchased items. 
+     */
+    void readPurchasedItems(); 
 
 }
