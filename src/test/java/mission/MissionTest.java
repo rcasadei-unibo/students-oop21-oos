31226f28ac70b80c804f2e9377bad1cc3aa1ab12
Davide Tonelli <davide.tonelli8@studio.unibo.it>
2022-04-19 19:33:26 +0200

    Add JUnit missions test

---
+++ src/test/java/mission/MissionTest.java
@@ -0,0 +1,99 @@
+package mission;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import model.Model;
+import model.ModelImpl;
+import model.Player;
+import model.Statistics;
+import model.mission.Mission;
+import model.mission.MissionFactoryImpl;
+import model.mission.MissionManager;
+import sound.SoundFactoryImpl;
+
+class MissionTest {
+
+    private static final int REWARD_AMOUNT = 20;
+    private static final int MAX_DISTANCE_MISSION = 1000;
+    private static final int MIN_DISTANCE_MISSION = 200;
+    private static final int MAX_COINS_TO_COLLECT_MISSION = 50;
+    private static final int MIN_COINS_TO_COLLECT_MISSION = 20;
+    private static final int MAX_JUMP_NUMBER_MISSION = 50;
+    private static final int MIN_JUMP_NUMBER_MISSION = 20;
+
+    private Model model;
+    private MissionManager missionManager;
+    private Statistics statistics;
+    private Player player;
+
+    @BeforeEach
+    public void init() {
+        this.model = new ModelImpl(new SoundFactoryImpl());
+        this.missionManager = this.model.getMissionManager();
+        this.statistics = this.model.getStatistics();
+        this.player = this.model.getGameState().getPlayer();
+    }
+
+    @Test
+    public void testCreateMission() {
+        final Optional<Mission> mission = this.missionManager.getMission();
+        assertTrue(mission.isPresent());
+        assertEquals(0, mission.get().getCounter());
+    }
+
+    @Test
+    public void testDistanceMission() {
+        final Mission mission = new MissionFactoryImpl(this.model).createDistanceMission();
+        assertFalse(mission.isCompleted());
+        int distance = 0;
+        while (distance < MAX_DISTANCE_MISSION && !mission.isCompleted()) {
+            this.statistics.update();
+            mission.updateCounter();
+            distance = this.statistics.getDistance();
+        }
+        assertTrue(mission.isCompleted());
+        assertTrue(mission.getCounter() >= MIN_DISTANCE_MISSION && mission.getCounter() <= MAX_DISTANCE_MISSION);
+        assertEquals(this.statistics.getGameCoins() + REWARD_AMOUNT, mission.getReward() + this.statistics.getGameCoins());
+    }
+
+    @Test
+    public void testCoinMission() {
+        final Mission mission = new MissionFactoryImpl(this.model).createCollectedCoinMission();
+        assertFalse(mission.isCompleted());
+        int coinCounter = 0;
+        while (coinCounter < MAX_COINS_TO_COLLECT_MISSION && !mission.isCompleted()) {
+            this.statistics.increaseCoin(1);
+            mission.updateCounter();
+            coinCounter++;
+        }
+        assertTrue(mission.isCompleted());
+        assertTrue(mission.getCounter() >= MIN_COINS_TO_COLLECT_MISSION && mission.getCounter() <= MAX_COINS_TO_COLLECT_MISSION);
+        assertEquals(this.statistics.getGameCoins() + REWARD_AMOUNT, mission.getReward() + this.statistics.getGameCoins());
+    }
+
+    @Test
+    public void testJumpMission() {
+        final Mission mission = new MissionFactoryImpl(this.model).createNumberOfJumpMission();
+        assertFalse(mission.isCompleted());
+        int jumpCounter = 0;
+        while (jumpCounter < MAX_JUMP_NUMBER_MISSION && !mission.isCompleted()) {
+            this.player.jump();
+            while (this.player.isJumping()) {
+                this.player.updateJump();
+            }
+            mission.updateCounter();
+            jumpCounter++;
+        }
+        assertTrue(mission.isCompleted());
+        assertTrue(mission.getCounter() >= MIN_JUMP_NUMBER_MISSION && mission.getCounter() <= MAX_JUMP_NUMBER_MISSION);
+        assertEquals(this.statistics.getGameCoins() + REWARD_AMOUNT, mission.getReward() + this.statistics.getGameCoins());
+    }
+
+}
