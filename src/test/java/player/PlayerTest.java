b523f35f51696b723ef4307e1f7dca65ee08fded
Sara Cappelletti <sara.cappelletti4@studio.unibo.it>
2022-04-20 15:26:54 +0200

    Add new tests

--- src/test/java/player/PlayerTest.java
+++ src/test/java/player/PlayerTest.java
@@ -1,52 +1,52 @@
 package player;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
 import javafx.geometry.Rectangle2D;
+import model.player.JumpState;
 import model.player.Player;
 import model.player.PlayerImpl;
 import sound.SoundFactoryImpl;
 
-public class PlayerTest {
+class PlayerTest {
 
     private final Player player = new PlayerImpl(new SoundFactoryImpl());
 
     @Test
-    public void testJump() {
+    void testJump() {
         /*Make the player jump, the fields isJumping must be true, 
          * and the field isGoingDown must be false*/
         player.jump();
-        assertTrue(player.isJumping());
-        assertFalse(player.isGoingDown());
+        assertEquals(JumpState.UP, player.getJumpState());
     }
 
     @Test
-    public void testUpdateJump1() {
+    void testUpdateJump1() {
         player.jump();
         player.updateJump();
         assertTrue(player.getBounds().getMinY() < PlayerImpl.LAND - PlayerImpl.MAIN_CHARACTER_HEIGHT);
-        assertFalse(player.isGoingDown());
+        assertNotEquals(JumpState.DOWN, player.getJumpState());
     }
 
     @Test
-    public void testUpdateJump2() {
+    void testUpdateJump2() {
         player.updateJump();
         assertEquals(player.getBounds().getMinY(), PlayerImpl.LAND - PlayerImpl.MAIN_CHARACTER_HEIGHT);
-        assertFalse(player.isJumping());
+        assertEquals(JumpState.NOT_JUMPING, player.getJumpState());
     }
 
     @Test
-    public void testLifes() {
+    void testLifes() {
         player.setNumberOfLives(2);
         assertEquals(player.getLives(), 3);
     }
 
     @Test
-    public void testJumpCounter() {
+    void testJumpCounter() {
         for (int i = 0; i < 4; i++) {
             player.jump();
             player.updateJump();
@@ -58,7 +58,7 @@ public class PlayerTest {
     }
 
     @Test
-    public void testBounds() {
+   void testBounds() {
         assertEquals(player.getBounds(), new Rectangle2D(PlayerImpl.PLAYER_X,
                                                          PlayerImpl.LAND - PlayerImpl.MAIN_CHARACTER_HEIGHT,
                                                          PlayerImpl.MAIN_CHARACTER_WIDTH, 
