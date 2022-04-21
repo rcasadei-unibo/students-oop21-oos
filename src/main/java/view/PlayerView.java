e7ef1bbaca090ac30d97e66bcfb91ccfba7a7631
Sara Cappelletti <sara.cappelletti4@studio.unibo.it>
2022-04-21 14:25:04 +0200

    Remove imagePath

--- src/main/java/view/PlayerView.java
+++ src/main/java/view/PlayerView.java
@@ -6,10 +6,4 @@ public interface PlayerView {
      * Render method.
      */
     void render();
-
-    /**
-     * Render method.
-     * @param path the new image path
-     */
-    void setImage(String path);
 }
--- src/main/java/view/PlayerViewImpl.java
+++ src/main/java/view/PlayerViewImpl.java
@@ -13,86 +13,86 @@ import model.player.PlayerImpl;
 
 public final class PlayerViewImpl implements PlayerView {
 
-    private final Pane pane; 
-
     private static final byte SPRITE_CHANGE = 40;
-    private static final int IMAGE1 = 131;
-    private static final int IMAGE2 = 175;
-    private static final int IMAGE3 = 221;
-    private static final int IMAGE4 = 252;
-    private static final int IMAGE5 = 307;
-    private static final int HEIGHT = 208;
-    private static final int IMAGE_WIDTH = 35;
-    private static final int IMAGE_HEIGHT = 45;
+    private static final int IMAGE1 = 15; 
+    private static final int IMAGE2 = 120;
+    private static final int IMAGE3 = 225;
+    private static final int IMAGE4 = 330;
+    private static final int IMAGE5 = 450;
+    private static final int HEIGHT = 20;
+    private static final int IMAGE_WIDTH = 90;
+    private static final int IMAGE_HEIGHT = 140; 
 
-    private String imagePath;
-
-    //coordinate dello sprite nel png per scegliere l'orso che mi serve
-    private int spriteX;
-    private int spriteY;
-    private JumpState currentDirection;
+    private final Pane pane; 
+    private final Player player;
+    private JumpState currentState;
     private byte currentSprite;
     private byte currentSpriteChange;
-
     private final Map<JumpState, int[]> spriteXCoordinates = new HashMap<>();
     private final Map<JumpState, int[]> spriteYCoordinates = new HashMap<>();
-
-    private final Player player;
-    private ImageView lastSpriteImage;
+    private int spriteX;
+    private int spriteY;
+    private ImageView spriteImage;
 
     public PlayerViewImpl(final Pane pane, final Player pl) {
         this.pane = pane;
         this.player = pl;
-        this.currentDirection = JumpState.NOT_JUMPING;
+        this.currentState = JumpState.NOT_JUMPING;
         this.currentSprite = 0;
         this.currentSpriteChange = 0;
-        this.imagePath = "Player2giusto.png";
 
         spriteXCoordinates.put(JumpState.NOT_JUMPING, new int[] {IMAGE1, IMAGE2, IMAGE3});
         spriteYCoordinates.put(JumpState.NOT_JUMPING, new int[] {HEIGHT, HEIGHT, HEIGHT});
         spriteXCoordinates.put(JumpState.UP, new int[] {IMAGE4});
-        spriteYCoordinates.put(JumpState.UP, new int[] {210});
+        spriteYCoordinates.put(JumpState.UP, new int[] {HEIGHT});
         spriteXCoordinates.put(JumpState.DOWN, new int[] {IMAGE5});
         spriteYCoordinates.put(JumpState.DOWN, new int[] {HEIGHT});
     }
 
+    /**
+     * This method changes the image based on the JumpState, and animate the NOT_JUMPING
+     * state with three different images.
+     */
     private void animate() {
-        if (this.player.getJumpState() == JumpState.UP && this.currentDirection != JumpState.UP) {
-            this.changeDirection(JumpState.UP);
-        } else if (this.player.getJumpState() == JumpState.NOT_JUMPING 
-                    && this.currentDirection != JumpState.NOT_JUMPING) {
-            this.changeDirection(JumpState.NOT_JUMPING);
-        } else if (this.player.getJumpState() == JumpState.DOWN && this.currentDirection != JumpState.DOWN) {
-            this.changeDirection(JumpState.DOWN);
-        } else {
+        if (!this.changeState()) {
             this.currentSpriteChange++;
                 if (currentSpriteChange >= SPRITE_CHANGE) {
                         currentSprite = (byte) ((currentSprite + 1) 
-                        % spriteXCoordinates.get(currentDirection).length);
+                        % spriteXCoordinates.get(currentState).length);
                         currentSpriteChange = 0;
                 }
         }
-        spriteX = spriteXCoordinates.get(this.currentDirection)[this.currentSprite];
-        spriteY = spriteYCoordinates.get(this.currentDirection)[this.currentSprite];
+        spriteX = spriteXCoordinates.get(this.currentState)[this.currentSprite];
+        spriteY = spriteYCoordinates.get(this.currentState)[this.currentSprite];
     }
 
-    private void changeDirection(final JumpState dir) {
-        this.currentDirection = dir;
-        this.currentSprite = 0;
-        this.currentSpriteChange = 0;
+    /**
+     * Change the curentState if it's different from player's state.
+     * @return true if currentState have been changed
+     */
+    private boolean changeState() {
+        if (this.player.getJumpState() != this.currentState) {
+            this.currentState = this.player.getJumpState();
+            this.currentSprite = 0;
+            this.currentSpriteChange = 0;
+            return true;
+        }
+        return false;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void render() {
-        this.pane.getChildren().remove(lastSpriteImage);
+        this.pane.getChildren().remove(spriteImage);
         this.animate();
-        this.lastSpriteImage = this.createImage(player);
-        //dire quale è la sprite imagine da mettere
-        this.pane.getChildren().add(this.lastSpriteImage);
+        this.spriteImage = this.createImage(player);
+        this.pane.getChildren().add(this.spriteImage);
     }
 
     private ImageView createImage(final Player pl) {
-        final ImageView image = new ImageView(new Image(imagePath));
+        final ImageView image = new ImageView(pl.getImage());
         image.setViewport(new Rectangle2D(spriteX, spriteY, IMAGE_WIDTH, IMAGE_HEIGHT));
         image.setPreserveRatio(true);
         image.setX(pl.getBounds().getMinX());
@@ -100,10 +100,5 @@ public final class PlayerViewImpl implements PlayerView {
         image.setFitHeight(PlayerImpl.MAIN_CHARACTER_HEIGHT);
         image.setFitWidth(PlayerImpl.MAIN_CHARACTER_WIDTH);
         return image;
-    }
-
-    @Override
-    public void setImage(final String path) {
-        this.imagePath = path;
     }
 }
