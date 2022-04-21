9e59f2db78104834e86ff0c54e6aa79621b80076
Fabio Veroli <fabio.veroli@studio.unibo.it>
2022-04-22 01:15:44 +0200

    Remove test that fail due to random entity generation

--- src/test/java/entity/manager/ManagerTest.java
+++ src/test/java/entity/manager/ManagerTest.java
@@ -1,6 +1,7 @@
 package entity.manager;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
@@ -26,9 +27,9 @@ class ManagerTest {
 
     private static final int NUM_ITERATIONS = 10_000;
     private static final int MIN_ENTITIES = 3;
-    private static final int RESULT_ONE = 5;
+    private static final int RESULT_ONE = 6;
+    private static final int OUT_OF_SCREEN = 1200;
 
-    private List<DynamicEntity> entities;
     private EntityManager generator;
     private EntityFactory factory;
     private GameInfo info;
@@ -45,10 +46,11 @@ class ManagerTest {
         final Dimension2D dimension = new Dimension2D(info.getWidth(), info.getHeight());
         generator = new EntityManagerImpl(dimension);
         factory = new EntityFactoryImpl(dimension);
-        entities  = generator.getEntities();
     }
+
     @Test
     void testGetter() {
+        final List<DynamicEntity> entities = generator.getEntities();
         /*Empty list at the beginning*/
         assertEquals(entities.size(), 0);
         /*Add some entities using the factory*/
@@ -56,7 +58,7 @@ class ManagerTest {
         entities.addAll(factory.combineAll(SpawnLevel.ONE, SpawnLevel.ZERO, SpawnLevel.TWO));
         entities.addAll(factory.combineObstacleCoin(SpawnLevel.ZERO, SpawnLevel.ONE));
         /*Check if the size of the list as changed*/
-        assertEquals(entities.size() - 1, RESULT_ONE);
+        assertEquals(entities.size(), RESULT_ONE);
     }
 
     @Test
@@ -64,26 +66,14 @@ class ManagerTest {
         /*Update the generator, if the size of the list is zero the test should fail*/
         for (int i = 0; i < NUM_ITERATIONS; i++) {
             generator.updateList();
-            if (entities.isEmpty()) {
-                fail("Entities list's size should be positive.");
-            }
-        }
-        /*Set all the entities to hit, when the list get updated the size should be lower then the previous one*/
-        final int entitiesSizeBefore = entities.size();
-        entities.forEach(e -> e.hit(true));
-        generator.updateList();
-        assertTrue(entities.size() <= entitiesSizeBefore);
-        /*Update the generator, if the size of the list is zero the test should fail*/
-        for (int i = 0; i < NUM_ITERATIONS; i++) {
-            generator.updateList();
-            if (entities.isEmpty()) {
+            if (generator.getEntities().isEmpty()) {
                 fail("Entities list's size should be positive.");
             }
         }
         /*Change the speed of the entities so they go out of screen, when the list get updated 
          * all the entities must have been removed except for the new added ones*/
-        generator.setSpeedX(info.getWidth() * 2);
+        generator.setSpeedX(OUT_OF_SCREEN);
         generator.updateList();
-        assertTrue(entities.size() <= MIN_ENTITIES);
+        assertTrue(generator.getEntities().size() <= MIN_ENTITIES);
     }
 }
