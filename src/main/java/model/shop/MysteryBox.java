ca2daf4496e0c1779808f5c2baed387cbe05f101
Rachele Margutti <rachele.margutti@studio.unibo.it>
2022-04-21 15:45:28 +0200

    Refactor MysteryBox

--- src/main/java/model/shop/MysteryBox.java
+++ src/main/java/model/shop/MysteryBox.java
@@ -1,6 +1,5 @@
 package model.shop;
 
-import model.Model;
 import model.Statistics;
 
 public interface MysteryBox {
@@ -8,10 +7,9 @@ public interface MysteryBox {
     /**
      * Create the prize of the mystery box, choosing between four different options.
      * @param stats the game statistics. 
-     * @param model the game model. 
      * @return the string with the prize message. 
      */
-    String createPrize(Statistics stats, Model model); 
+    String createPrize(Statistics stats); 
 
     /**
      * 
--- src/main/java/model/shop/MysteryBoxImpl.java
+++ src/main/java/model/shop/MysteryBoxImpl.java
@@ -2,7 +2,6 @@ package model.shop;
 
 import java.util.Random;
 
-import model.Model;
 import model.Statistics;
 
 public class MysteryBoxImpl implements MysteryBox {
@@ -10,9 +9,10 @@ public class MysteryBoxImpl implements MysteryBox {
     private final int price; 
     private final Random rand = new Random(); 
 
-    private static final int BOX_PRICE = 100; 
+    private static final int BOX_PRICE = 200; 
     private static final int PRIZES_NUM = 3; 
-    private static final int MONEY_PRIZE = 200; 
+    private static final int MONEY_PRIZE1 = 500; 
+    private static final int MONEY_PRIZE2 = 1000; 
 
     public MysteryBoxImpl() {
         this.price = BOX_PRICE; 
@@ -28,20 +28,20 @@ public class MysteryBoxImpl implements MysteryBox {
     /**
      * {@inheritDoc}
      */
-    public final String createPrize(final Statistics stats, final Model model) {
+    public final String createPrize(final Statistics stats) {
         final int random = rand.nextInt(PRIZES_NUM); 
         String message; 
         switch (random) {
             case 0: 
-                stats.setTotalCoins(stats.getTotalCoins() + MONEY_PRIZE);
-                message = "Congratulation! You won 1000 coins!"; 
+                stats.setTotalCoins(stats.getTotalCoins() + MONEY_PRIZE1);
+                message = "Congratulation! You won 500 coins!"; 
                 break; 
             case 1: 
                 message = "You can't always be lucky, you've just paid for nothing"; 
                 break; 
             case 2: 
-                model.getGameState().getPlayer().setNumberOfLives(1);
-                message = "Yeah! You've just gained an extra life!!!"; 
+                stats.setTotalCoins(stats.getTotalCoins() + MONEY_PRIZE2);
+                message = "Yeah! You've just gained 1000 coins!!!"; 
                 break; 
             default: 
                 message = "Error: selected prize does not exist"; 
